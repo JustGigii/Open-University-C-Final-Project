9961f6dd875b9f396a789f50fe2806222fb5ec33
omri gigi <gigiomri@gmail.com>
2024-11-14 18:04:45 +0200

    fix macro system

--- source/MacroSystem.c
+++ source/MacroSystem.c
@@ -12,8 +12,15 @@ LinePtr InitMacro(LinePtr head)
     int succsec;
     int indextofree;
     while (temp->next)
-    {
-       if (strstr(temp->next->line, "mcro"))
+    { 
+
+         
+        add = ExistMacro(macroarray, macroindex, temp->next->line);
+        if (add)
+        {
+           temp = AddMacroToProgram(temp, add);
+        }
+       else if (strstr(temp->next->line, "mcro"))
         {
       
             macroarray = addMacroToList(macroarray, macroindex, temp);
@@ -23,17 +30,6 @@ LinePtr InitMacro(LinePtr head)
             temp = temp->next;
     }
     temp = head;
-    while (temp->next)
-    {
-        
-        add = ExistMacro(macroarray, macroindex, temp->next->line);
-        if (add)
-        {
-           temp = AddMacroToProgram(temp, add);
-        }
-        else
-             temp = temp->next;
-    }
     RecountLine(head, START_LINE);
     PrintLines(head);
     return head;
@@ -59,31 +55,21 @@ macroPtr *addMacroToList(macroPtr *macroarray, int size, LinePtr temp)
     int indexname;
     char **split;
     int sizeofsplit = 0;
-    
     if (size == 0)
-    {
-        macroarraynew = malloc(sizeof(macroPtr));
-    }
-    else
-    {
-        macroarraynew = realloc(*macroarray, (size + 1) * sizeof(macroPtr));
-    }
+        macroarraynew = malloc(sizeof(macroPtr));  
+    else  
+        macroarraynew = realloc(macroarray, (size + 1) * sizeof(macroPtr));
+
 
     if (!macroarraynew)
         return NULL;
+   
     macroarraynew[size] = malloc(sizeof(macrostruct));
     split = Split(temp->next->line, " ", &sizeofsplit);
     indexname = getmacroname(split, sizeofsplit);
     if(indexname == -1)
         return NULL;
-    macroarraynew[size]->name = malloc((strlen(split[indexname]) + 1) * sizeof(char));
-    if( macroarraynew[size]->name  == NULL)
-    {
-        free(macroarraynew[size]);
-        free(macroarraynew);
-        return NULL;
-    }
-    macroarraynew[size]->name = strcpy(macroarraynew[size]->name, split[indexname]);
+    macroarraynew[size]->name = strdup(split[indexname]);
     macroarraynew[size]->start = InitSingelMacro(temp);
     if (macroarraynew[size]->start == NULL)
         return NULL;
@@ -98,15 +84,15 @@ LinePtr InitSingelMacro(LinePtr copy)
     copy->next = tofree->next;
     freeLine(tofree);
     macro= copy->next;
+   
     while (strcmp(copy->next->line, "mcroend\n") != 0)
     {
         copy = copy->next;
     }
     tofree = copy->next;
-    freeLine(tofree);
     copy->next = NULL;
     head->next = tofree->next;
-
+    freeLine(tofree);
     return macro;
 }
 
@@ -128,19 +114,19 @@ LinePtr AddMacroToProgram(LinePtr temp, LinePtr list)
 {
     LinePtr newLine;
     LinePtr prev = temp;
-    LinePtr next = temp->next;
     newLine = temp->next;
     temp->next = newLine->next;
     freeLine(newLine);
+    LinePtr next = temp->next;
     while (list)
     {
-        newLine = InitLine(list->line, list->lineNum);
+        newLine = InitLine(list->line,-1);
         prev->next = newLine;
         prev= prev->next;
         list = list->next;
 
     }
      prev->next = next;
-    return next;
+    return prev;
     
 }
