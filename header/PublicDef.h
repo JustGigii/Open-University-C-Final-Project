61fc19f81352f62696b855385a90de237b78044f
omri gigi <gigiomri@gmail.com>
2025-01-07 09:04:28 +0200

    start buil thefunction

--- header/PublicDef.h
+++ header/PublicDef.h
@@ -15,7 +15,26 @@ typedef enum
  SUCCESS = 0,
  FAILURE = 1,
  FAILURE_OPRAND_NOT_MUCH = 2,
- FAILURE_TO_MUCH_LANE_TO_OPERAND = 3
+ FAILURE_TO_MUCH_LANE_TO_OPERAND = 3,
+ FAILURE_OUT_OF_RANGE =4
 
 } SATATUS;
+ typedef enum {
+   ARE =0,
+   funct =3,
+   RD= 8, /*Destination register*/
+   DS = 11, /*Destination sorting*/
+   RS = 13, /*Source register*/
+   SS = 11, /*Source sorting*/
+   OPCODE = 18,
+}BIT_START;
+ typedef enum {
+   ARE =3,
+   funct =5,
+   RD= 3, /*Destination register*/
+   DS = 2, /*Destination sorting*/
+   RS = 3, /*Source register*/
+   SS = 2, /*Source sorting*/
+   OPCODE = 6,
+}BIT_LENGHT;
 #endif
--- source/cheackSentece.c
+++ source/cheackSentece.c
@@ -1,14 +1,43 @@
 #include "../header/cheackSentece.h"
 
+unsigned int extract_bits(int value, BIT_START start_bit, BIT_LENGHT length,SATATUS *peola) {
+    /* Ensure start_bit and length are within valid range */
+    if (start_bit < 0 || start_bit >= 24 || length <= 0 || start_bit + length > 24) {
+        *peola = FAILURE_OUT_OF_RANGE;
+        return 0; /* Return 0 for invalid input */
+    }
 
+    /* Create a mask with the desired length of 1s */
+    unsigned int mask = (1 << length) - 1;
 
-SATATUS cheackSentece(char **words, int sizewords,labelPtr ** tables, int* tablesize ,int *instructionCount, int * asembleyline)
+    /* Shift the mask to the starting position and apply it to the value */
+    return (value >> start_bit) & mask;
+}
+
+void print_binary(unsigned int value, int num_bits) {
+    int i;
+    for (i = num_bits - 1; i >= 0; i--) {
+        printf("%d", (value >> i) & 1);
+    }
+    printf("\n");
+}
+SATATUS cheackSentece(char **words, int sizewords,labelPtr ** tables, int* tablesize ,int *instructionCount, int ** asembleyline)
 {
+    int opcode;
+    SATATUS peola = SUCCESS;
     switch (sizewords)
     {
     case 1:
-        if(strcmp(words[0], "data") == 0||strcmp(words[0],""))
-        /* code */
+        if(strcmp(words[0], "rts") == 0||strcmp(words[0],"stop")==0)
+        {
+
+            *asembleyline = malloc(sizeof(int));
+           **asembleyline = 4;
+           opcode = (strcmp(words[0], "rts") == 0) ? 14 : 15;
+           **asembleyline = extract_bits(opcode,OP,opcode,&peola)
+        }
+        else 
+        return FAILURE_OPRAND_NOT_MUCH;
         break;
     case 2:
     break;
